80ba4541c6de2809e5d656f53603360b999fcd24
RijansPatoliya <rijans.patoliya.cg@gmail.com>
2024-12-05 16:12:14 +0530

    spotify with es6 feature and seprate code

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+// import MainBody from './components/MainBody.jsx'
+// import PlayControl from './components/PlayControl.jsx'
+
+
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <div>App</div>
+//   )
+// }
+
+// export default App
---
+++ vite-project/src/components/PlayControl.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const PlayControl = () => {
+  return (
+    <>
+    <div id='footer'>
+      <div className="line1">
+           <img src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/Music%20Player%20Options.png?raw=true"/>
+            <div className="rgt">
+            <img src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/Right%20options.png?raw=true"/>
+            </div>
+      </div>
+      <div className="line2">
+          <p>Listening on ASUS-RG-G5G3GT</p>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default PlayControl
